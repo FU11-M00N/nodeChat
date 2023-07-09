1b19a94bd28f37638c2e0a3523ce4ef51ce76637
FU11-M00N <mmmhobin3@gmail.com>
2023-07-09 11:29:51 +0900

    feat: 채팅방 닉네임 출력

--- socket.js
+++ socket.js
@@ -30,18 +30,27 @@ module.exports = (server, app, sessionMiddleware) => {
 
    chat.on('connection', socket => {
       const req = socket.request;
-      let roomNum = '';
-      socket.on('join', roomId => {
-         roomNum = roomId;
 
-         console.log(roomNum, ' 번 chat 네임스페이스 접속 ');
+      let roomNum = '';
+      // 채팅창에 들어 왔을 때
+      socket.on('join', data => {
+         const roomId = data.roomId;
+         roomNum = roomId;
+         // 참여 인원 초기화
+         if (!socket.adapter.rooms.get('room' + roomNum)) {
+            socket.adapter.rooms.set('room' + roomNum, []);
+         }
+         socket.adapter.rooms.get('room' + roomNum).push(req.session.user.username);
+
          socket.join(roomNum);
          chat.to(roomNum).emit('join', {
             user: 'system',
             chat: `${req.session.user.username} 님이 접속하셨습니다.`,
+            cnt: socket.adapter.rooms.get(roomNum)?.size,
+            connectedUsers: socket.adapter.rooms.get('room' + roomNum),
          });
       });
-
+      // 채팅 보냈을때
       socket.on('chat', msg => {
          socket.join(roomNum);
          chat.to(roomNum).emit('chat', {
@@ -49,12 +58,26 @@ module.exports = (server, app, sessionMiddleware) => {
             chat: `${req.session.user.username} : ${msg}`,
          });
       });
-
+      socket.on('setNickName', data => {
+         connectedUsers.push(data);
+         socket.join(roomNum);
+         chat.to(roomNum).emit('setNickName', {
+            connectedUsers,
+         });
+      });
+      // 채팅 창을 나갔을 때
       socket.on('disconnect', () => {
-         console.log('chat 네임스페이스 접속 해제');
+         // 해당 유저의 닉네임의 위치를 찾음
+         const username = req.session.user.username;
+         const findIndex = socket.adapter.rooms.get('room' + roomNum)?.indexOf(username);
+         if (findIndex > -1) {
+            socket.adapter.rooms.get('room' + roomNum).splice(findIndex, 1);
+         }
+
          socket.to(roomNum).emit('exit', {
             user: 'system',
-            chat: `${req.session.user.username}님이 퇴장하셨습니다.`,
+            chat: `${username}님이 퇴장하셨습니다.`,
+            connectedUsers: socket.adapter.rooms.get('room' + roomNum),
          });
       });
    });
